5ba18ac19cd17272dca5d078c8aa81f171e4dc18
alevenskiy <ppcrn97@gmail.com>
2021-03-29 17:59:28 +0300

    Добавьте файлы проекта.

---
+++ Multi/Multi.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a;
+    float b;
+    cout << "a * b = c\n";
+
+    cout << "float a = ";
+    cin >> a;
+
+    cout << "\nfloat b = ";
+    cin >> b;
+
+    cout << "\na * b = " << a * b;
+}
+
